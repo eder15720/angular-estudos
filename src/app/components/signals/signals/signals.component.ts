05e04ae98690277c0e0256f081e5a238b052fcf1
eder15720 <eder15720@hotmail.com>
2024-04-14 19:52:14 -0300

    adicionando estudo com signals ao projeto

--- src/app/components/signals/signals/signals.component.ts
+++ src/app/components/signals/signals/signals.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { TemaplteControlFlowComponent } from '../../template/temaplte-control-flow/temaplte-control-flow.component';
 
 @Component({
@@ -19,7 +19,32 @@ export class SignalsComponent {
 
   public array = signal([1]);
 
-  constructor(){}
+  /* 
+    effect - raramente são necessários na maioria dos códigos, mas podem ser úteis
+    em circunstancias específicas.
+
+    - Registro de dados sendo exibidos e quando eles mudam, seja para análise
+    ou como ferramenta de depuração.
+
+    - Manter os dados sincronizados com window.localStorage.
+
+    - Adicionando comportamento DOM personalizado que não pode ser expresso com
+    sintaxe de modelo.
+
+    - Executar renderização personalizada em um <canvas>, biblioteca de gráficos
+    ou outra biblioteca de UI de terceiros.
+
+  */
+
+  constructor(){
+    effect(() => {
+      console.log(this.firstName())
+      if(this.firstName() == "Joao"){
+        alert();
+      }
+      console.log(this.array())
+    })
+  }
 
   public updateName(){
     return this.firstName.set("Joao");
