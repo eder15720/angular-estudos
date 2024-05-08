a3b3f1fa9fb1dff3cbf15139bd59c1aab8ef7088
eder15720 <eder15720@hotmail.com>
2024-05-08 00:16:05 -0300

    adicionando funcionalidade para exibição de dados com output

--- src/app/components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component.ts
+++ src/app/components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component.ts
@@ -12,4 +12,5 @@ import { OutputComponent } from '../output/output.component';
 })
 export class PaiOuMaeComponent {
   public name = signal("Henrique Eder com signal referenciado");
+  public outputName = signal("Output: sem valor");
 }
